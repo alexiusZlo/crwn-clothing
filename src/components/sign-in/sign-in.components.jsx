eab37df05deb8eb4e53522771e8ac74f4fc29f26
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-15 16:06:40 +0300

    have been created form sign-in initial template

---
+++ src/components/sign-in/sign-in.components.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input,component";
+
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target;
+
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password.</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email"
+                               name='email'
+                               value={this.state.email}
+                               handleChange={this.handleChange}
+                               label='email'
+                               required/>
+                    <FormInput type="password"
+                               name='password'
+                               value={this.state.password}
+                               handleChange={this.handleChange}
+                               label='password'
+                               required/>
+                    <input type="submit" value='submit'/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
