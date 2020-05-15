d692f64f467abb16fa89a9693efe1b0c80cef0ae
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-15 11:17:58 +0300

    have been added router lib

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Router, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+    <div>1</div>
+)
 
 function App() {
     return (
         <div>
-            <HomePage/>
+            <Switch>
+                <HomePage/>
+                <Router exact path='/' component={HomePage}/>
+                <Router path='/hats' component={HatsPage}/>
+            </Switch>
         </div>
     );
 }
