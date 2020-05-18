4341caf0fed3d70338001d6f808d1009363b3009
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-19 01:28:03 +0300

    finishing sign in

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input,component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import {signInWithGoogle} from "../../firebase/firebasse.utils";
+import {auth, signInWithGoogle} from "../../firebase/firebasse.utils";
 
 import './sign-in.styles.scss';
 
@@ -23,9 +23,18 @@ class SignIn extends React.Component {
         this.setState({[name]: value})
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''})
+
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email: '', password: ''})
+        } catch (e) {
+            console.log(e)
+        }
+
     }
 
     render() {
