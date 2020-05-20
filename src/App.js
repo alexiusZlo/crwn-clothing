10a9b4aa1dbbdfee16fb3faf6cef75a09980650e
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-20 14:48:16 +0300

    updating app to home if user sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux'
 
 import './App.css';
@@ -7,7 +7,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from "./components/header/header.component";
-import SignInAndSingUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import SignInAndSingUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from "./firebase/firebasse.utils";
 import {setCurrentUser} from "./redux/user/user.actions";
 
@@ -36,21 +36,34 @@ class App extends React.Component {
     }
 
     render() {
+        console.log(this.props.currentUser)
         return (
             <div>
                 <Header/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSingUp}/>
+                    <Route
+                        exact
+                        path='/signin'
+                        render={() =>
+                            this.props.currentUser ?
+                                (<Redirect to='/'/>) :
+                                (<SignInAndSingUpPage/>)
+                        }
+                    />
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,15 +9,14 @@ import './header.styles.scss'
 const Header = ({currentuser}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
-            <Logo className='logo' />
+            <Logo className='logo'/>
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>Shop</Link>
             <Link className='option' to='/shop'>Contact</Link>
-            {
-                currentuser ?
-                    <div className='option' onClick={()=> auth.signOut()}>Sign out</div>
-                    : <Link className='option' to='/signin'>Sign in</Link>
+            {currentuser ?
+                <div className='option' onClick={() => auth.signOut()}>Sign out</div>
+                : <Link className='option' to='/signin'>Sign in</Link>
             }
         </div>
     </div>
