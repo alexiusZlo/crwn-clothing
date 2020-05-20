b38489ff4ce800bf3782ef22578c2222517fea22
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-20 14:05:51 +0300

    have been added redux and added current user to store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 import {auth} from "../../firebase/firebasse.utils";
 
-import './header.styles.scss'
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import './header.styles.scss'
 
 const Header = ({currentuser}) => (
     <div className='header'>
@@ -22,4 +23,8 @@ const Header = ({currentuser}) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = statte => ({
+    currentUser: statte.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
