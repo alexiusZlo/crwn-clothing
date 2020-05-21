234618eba13b566d382e394f9f5343956810802c
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-21 16:59:32 +0300

    added createStructuredSelector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
+import {createStructuredSelector} from 'reselect'
 
 import {auth} from "../../firebase/firebasse.utils";
 
@@ -8,6 +9,8 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss'
 import CardIcon from "../card-icon/card-icon.components";
 import CardDropdown from "../card-dropdown/card-dropdown.component";
+import {selectCartHidden} from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/user/user.selector";
 
 const Header = ({currentUser, hidden}) => {
     return (
@@ -29,9 +32,9 @@ const Header = ({currentUser, hidden}) => {
     )
 }
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
