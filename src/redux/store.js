6f21ad29d54a183839c211da1a7c30a4b5406435
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-23 23:51:39 +0300

    have been added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore} from 'redux-persist'
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -11,4 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default  store ;
+export const persistor = persistStore(store);
+
+export default {store, persistor};
