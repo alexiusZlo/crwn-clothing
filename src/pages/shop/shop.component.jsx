eb80c21f95dfa2b317689cd0394d6c2cd63f1fec
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-24 17:13:04 +0300

    added collections-overview.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import {connect} from 'react-redux';
-import SHOP_DATA from "./shop.data";
+
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
 import './shop.styles.scss';
-import CollectionPreview from "../../components/collection-preview/collection-preview.components";
-import {createStructuredSelector} from "reselect";
-import {selectCollections} from "../../redux/shop/shop.selectors";
 
 const ShopPage = ({collections}) => (
     <div className='shop-page'>{
-        collections.map(({id, ...otherCollectonProps}) => (
-            <CollectionPreview key={id} {...otherCollectonProps} />
-        ))
+        <CollectionsOverview/>
     }</div>
 )
 
-const mapStateToProps =createStructuredSelector({
-    collections: selectCollections
-})
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
