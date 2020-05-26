3f1f1dad473c9215d0c66d4a476207991de38be6
alexey.zlobin@exposit.com <pwY9nt1lEVss>
2020-05-26 17:19:34 +0300

    have been added SpinnerWrapper to shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -13,7 +13,7 @@ const CollectionItem = ({item, addItem}) => {
                  style={{background: `url(${imageUrl})`}}/>
             <div className="collection-footer">
                 <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
+                <span className='price'>${price}</span>
             </div>
             <CustomButton className='custom-button' onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
         </div>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
-import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebasse.utils";
 import {connect} from "react-redux";
+
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebasse.utils";
 import {updateCollections} from "../../redux/shop/shop.actions";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+import CollectionPage from '../collection/collection.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
+    state = {
+        loading: true
+    };
+
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
@@ -17,6 +27,7 @@ class ShopPage extends React.Component {
         collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({loading: false});
         })
     }
 
@@ -26,12 +37,18 @@ class ShopPage extends React.Component {
 
     render() {
         const {match} = this.props
+        const {loading} = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+                <Route exact
+                       path={`${match.path}`}
+                       render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>}
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>}
+                />
             </div>
-
         )
     }
 }
